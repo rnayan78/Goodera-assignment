171e473f146ffb3f777e57d77480cb658f0c4a16
rnayan78 <56041285+rnayan78@users.noreply.github.com>
2021-06-04 20:25:52 +0530

    Update goodera.py
    Updated solution

--- goodera.py
+++ goodera.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+
 df = pd.read_excel(r'R:\Code\New folder (2)\Assignment_Excel.xlsx')  
 print(df)  #prints the current value 
-           
 
-df = df.assign(a=df['Output String'])  #changed the name of input string to 'a'
 
-print(df) #prints the new input value which is identical to output value
-df.to_excel("answer.xlsx")  #saves a new file in which input string identical to output
+df['a'] = df['a'].str.lstrip()
+df['a'] = df['a'].str.capitalize()
+
+
+print(df) #prints the new input value 
+#df.to_excel("answer.xlsx")  
